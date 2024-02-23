0d8ff18815b40a08bd21b2cfeff198389ecf6f71
unknown <dhoouglas@hotmail.com>
2024-02-23 12:28:37 -0300

    Router Post

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,26 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod';
 
 const app = fastify();
 
-app.get('/hello', () => {
-    return "Hello Nlw !"
+const prisma = new PrismaClient();
+
+
+app.post('/polls', async (request, replay) => {
+    const createPollsBody = z.object({
+        title: z.string(),
+    });
+
+    const { title } = createPollsBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    });
+
+    return replay.status(201).send({ pollId: poll.id });
 })
 
 app.listen({ port: 3333 }).then(() => {
